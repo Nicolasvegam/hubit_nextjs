0f6549b57eb1d281ec7ddda1da2110518b46c8b5
perezpefaur <perezpefaur@uc.cl>
2022-06-28 17:42:57 -0400

    Poniendo padding abajo cuando esta chico

--- pages/mecanicos/[id].js
+++ pages/mecanicos/[id].js
@@ -19,7 +19,7 @@ const MecanicPage = ( mecanic ) => {
           </div>
         </header>
         <main>
-        <div className="mt-12 max-w-2xl mx-auto px-4 sm:px-6 lg:max-w-7xl lg:px-8">
+        <div className="mt-12 max-w-2xl mx-auto px-4 sm:px-6 lg:max-w-7xl lg:px-8 sm:pb-16 pb-16 lg:pb-1">
           <div className="lg:grid lg:grid-cols-12 lg:auto-rows-min lg:gap-x-8">
             <div className="lg:col-start-8 lg:col-span-5">
               <div className="flex justify-between">
