b49e6b4b592733d085f7a88358eaaa2f9e3695ed
perezpefaur <perezpefaur@uc.cl>
2022-06-28 13:00:55 -0400

    Initial push

---
+++ pages/authPage.js
@@ -0,0 +1,24 @@
+import { useState, useEffect } from 'react'
+import { supabase } from '../utils/supabaseClient'
+import Auth from '../components/Auth'
+import Account from '../components/Account'
+
+
+const LogInPage = () => {
+    const [session, setSession] = useState(null)
+
+    useEffect(() => {
+        setSession(supabase.auth.session())
+
+        supabase.auth.onAuthStateChange((_event, session) => {
+        setSession(session)
+        })
+    }, [])
+
+    return (
+        <div className="container">
+            {!session ? <Auth /> : <Account key={session.user.id} session={session} />}
+        </div>
+    )
+}
+export default LogInPage
