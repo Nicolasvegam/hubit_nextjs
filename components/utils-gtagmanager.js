eb28851af503ffa6e091a164f2a3dcb8d567446f
Nicolasvegam <ndvega@uc.cl>
2022-07-04 19:43:01 -0400

    change tag manager

--- components/utils-gtagmanager.js
+++ components/utils-gtagmanager.js
@@ -9,7 +9,7 @@ export function ScriptTagManager() {
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-5T7MKSK');
+          })(window,document,'script','dataLayer','GTM-N63CNSL');
         `}
         </Script>
 
