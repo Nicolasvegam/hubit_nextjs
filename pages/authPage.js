bdf5228dc0f730aff3d302b9fb6dd2a8345d85a4
perezpefaur <perezpefaur@uc.cl>
2022-07-06 11:54:59 -0400

    Viendo si funciona ahora

--- pages/authPage.js
+++ pages/authPage.js
@@ -1,5 +1,7 @@
 import { useState, useEffect } from 'react'
 import { supabase } from '../utils/supabaseClient'
+import Auth from '../components/auth'
+import Account from '../components/account'
 
 
 const LogInPage = () => {
@@ -14,7 +16,7 @@ const LogInPage = () => {
 
     return (
         <div>
-            {!session ? <p /> : <p key={session.user.id} session={session} />}
+            {!session ? <Auth /> : <Account key={session.user.id} session={session} />}
         </div>
     )
 }
