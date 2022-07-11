e3cb94fb8c379a396ccf17b5199ee92d81e4ac92
perezpefaur <perezpefaur@uc.cl>
2022-07-11 16:56:32 -0400

    Se arregla styles

--- components/dashboard-suggestions.js
+++ components/dashboard-suggestions.js
@@ -16,7 +16,7 @@ export default function FutureServicesCard( { car } ) {
             <div className="block hover:bg-gray-50">
               <div className="px-4 py-4 sm:px-6">
                 <div className="flex items-center justify-between">
-                  <p className="text-sm font-medium text-indigo-600 truncate xl:w-[30vw] lg:w-[40vw] md:w-[53vw] sm:w-[70vw] w-[45vw]">{ service.name }</p>
+                  <p className="text-sm font-medium text-indigo-600 truncate xl:w-[30vw] lg:w-[40vw]">{ service.name }</p>
                   <div className="ml-2 flex-shrink-0 flex">
                     {service.status == "Realizado" ? <p className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">{ service.status }</p> :
                     service.status == "Agendado" ? <p className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-yellow-100 text-yellow-800">{ service.status }</p> :
